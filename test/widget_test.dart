0e8ee0324099d882f4bd9c94adbf4e41cb6a6f47
Yousef Hussein <yousefhussein779590@gmail.com>
2026-05-20 10:57:06 +0300

    chore(test): remove default placeholder widget test

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('money math stays predictable', () {
-    const debts = 125000.0;
-    const payments = 45000.0;
-    expect(debts - payments, 80000.0);
-  });
-}
